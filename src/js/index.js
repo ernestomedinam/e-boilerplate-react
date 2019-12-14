23494d2f18437f0b7b6abdf834c5962910e35bb0
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-12-14 18:10:14 -0400

    installed regenerator-runtime to support async-await, imported in index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(<Layout />, document.getElementById("root"));
